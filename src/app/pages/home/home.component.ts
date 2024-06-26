11e7a2d0f33ca9afb34595a26bfd7d197f8add85
a.temerkhanov <a.temerkhanov@kazdream.kz>
2024-06-26 17:02:20 +0500

    add search input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { Forecast } from '../../core/common';
+import { WeatherService } from '../../core/services/weather.service';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatCommonModule } from '@angular/material/core';
-import { WeatherService } from '../../core/services/weather.service';
-import { Observable } from 'rxjs';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +19,11 @@ import { Observable } from 'rxjs';
     DatePipe,
     MatCardModule,
     MatCommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatAutocompleteModule,
     NgFor,
     NgIf,
   ],
@@ -28,6 +37,7 @@ export class HomeComponent implements OnInit{
 
   public currentFC: any = null;
   public dailyFC: Forecast[] = [];
+  public cities: any[] = [];
 
   constructor(
     private weatherService: WeatherService,
@@ -38,12 +48,10 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     this.weatherService.getForcast({}).subscribe((r) => {
       this.currentFC = r[0];
-      console.log(this.currentFC);
     });
 
     this.weatherService.getForcast5Days({}).subscribe((r) => {
       this.dailyFC = r.DailyForecasts;
-      console.log(this.dailyFC);
     });
   }
 }
