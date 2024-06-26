fed44c69a0189d1562ece4fb714eb3ae3a6d2399
a.temerkhanov <a.temerkhanov@kazdream.kz>
2024-06-26 15:58:48 +0500

    add http client. get api key. add routes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './homeService';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    
+  ],
+  providers: [
+    HomeService,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  constructor(
+    private homeService: HomeService,
+  ) {}
+
+
+  ngOnInit(): void {
+    this.homeService.getForcast({}).subscribe((r) => {
+      console.log(r);
+    });
+  }
 }
